5015d6f5deaf680d182960f2eb540aab456e856d
nao18 <justdo.nao01@gmail.com>
2021-05-02 18:36:08 +0900

    first-commit

---
+++ app/src/main/java/com/example/janken/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.janken
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import com.example.janken.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val id = intent.getIntExtra("MY_HAND", 0)
+
+        binding.gu.setOnClickListener { onJankenButtonTapped(it) }
+        binding.choki.setOnClickListener { onJankenButtonTapped(it) }
+        binding.pa.setOnClickListener { onJankenButtonTapped(it) }
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit {
+            clear()
+        }
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND", view?.id)
+        startActivity(intent)
+    }
+}
